aba52a675facbe3768aecb899a0e23641a8db9a5
petar malamov <petar.malamov@softwaregroup.com>
2020-11-29 20:40:57 +0200

    remove hoc

--- lister2/Components/LoadingHoc/LoadingHoc.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable no-unused-vars */
-import React from 'react';
-
-const LoanHoc = (WrappedComponent) => (props) => {
-  return <WrappedComponent {...props} />;
-};
-
-export default LoanHoc;
